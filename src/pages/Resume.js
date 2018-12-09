4c092efff264a361cb94e921cfa3147e5f73e2f1
mfreema1 <mfreema1@stevens.edu>
2018-12-08 23:56:05 -0500

    Add resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -8,6 +8,9 @@ class Resume extends React.Component {
                 <Row>
                     <Col sm="12" md={{ size: 10, offset: 1 }}>
                         <Heading text={"My Resume"}/>
+                        <p>If you'd like to take a look at my formal qualifications, I'll throw you a link right <a href="../static/resume.pdf">here.</a></p>
+                        <p>I'll also throw in a text version of it below if you're not one for formatting.</p>
+                        <p>TODO: RESUME</p>
                     </Col>
                 </Row>
             </Container>
