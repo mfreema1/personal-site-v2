7621127a1f1f15b99ad80bd05794052ac2c2abc5
mfreema1 <mfreema1@stevens.edu>
2018-12-08 23:58:39 -0500

    Update heading to new styles

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Row, Col } from 'reactstrap';
 class Heading extends React.Component {
     render() {
         return (
             <div>
-                <h1 className="heading">{this.props.text}</h1>
+                <h1 className="big-text special-font pushed-down">{this.props.text}</h1>
                 <hr/>
             </div>
         )
