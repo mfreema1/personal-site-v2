88cda4b4a004128c2bd8bb978558013a69b31047
mfreema1 <mfreema1@stevens.edu>
2018-12-08 23:59:47 -0500

    Pull recent commits to page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,71 @@
 import React from 'react';
+import { Container } from 'reactstrap';
 import Intro from '../components/Intro';
 import ActivityAggregate from '../components/ActivityAggregate';
 
-const cards = [
-    {
-        title: "Card number one",
-        subtitle: "A very important card",
-        text: "Boy is this an important card.  We should throw some text in here to see how it looks."
-    },
-    {
-        title: "Card number two",
-        subtitle: "This is a very unimportant card",
-        text: "Please disregard this card.  It is of very little importance to anyone."
-    },
-    {
-        title: "Card number three",
-        subtitle: "This card is moderatel important I guess",
-        text: "We're not really sure where to put this one.  In some regards, it is important, but in some it's not.  Kind of a wild one."
-    },
-    {
-        title: "Card number four",
-        subtitle: "I don't even know for this one.",
-        text: "We're dumbfounded.  There's really no way for us tell with this, we really dropped the ball."
-    }
-]
+//sort repos based on last push
+const compare = (obj1, obj2) => {
+    if(obj1.pushed_at < obj2.pushed_at)
+        return 1
+    return -1
+}
 
 class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            repos: [],
+            commits: []
+        }
+    }
+
+    componentDidMount() {
+
+        //make two requests, one to the cache and one to get fresh data
+        //load the cached immediately, wait for any new data
+        self.addEventListener('fetch', (event) => {
+            event.respondWith(caches.open('commits')
+                .then((cache) => {
+                    return fetch(event.request)
+                        .then((fresh) => {
+                            cache.put(event.request, fresh.clone());
+                            return fresh;
+                        });
+                }));
+        });
+
+        fetch('https://api.github.com/users/mfreema1/repos')
+            .then((data) => { data.json()
+                .then((json) => {
+                    this.setState({ repos: json.sort(compare).slice(0, 3)}); //go get most recent three
+                });
+            });
+
+        fetch('https://api.github.com/users/mfreema1/events')
+            .then((data) => { data.json()
+                .then((json) => {
+                    //TODO: paginate the response
+                    const commits = (json.filter((e) => { return e.type === "PushEvent" }).map((e) => {
+                        return {
+                            repo: e.repo.name,
+                            createdAt: e.created_at,
+                            commits: e.payload.commits.map((c) => { return { message: c.message, url: c.url }})
+                        }
+                    }));
+                    this.setState({ commits: commits});
+                });
+            });
+    }
+
     render() {
         return (
-            <div>
+            <Container>
                 <Intro />
-                <ActivityAggregate cards={cards}/>
-            </div>
+                <ActivityAggregate cards={ this.state.commits }/>
+            </Container>
         )
     }
 }
-//
+
 export default Home;
