35b5185f985b3afdd7f05ca9bab8057c93bfe0ab
mfreema1 <mfreema1@stevens.edu>
2019-01-02 18:13:13 -0500

    Begin resume text page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import Heading from '../components/Heading';
 import { Container, Row, Col } from 'reactstrap';
+const resume = require('../misc/resume');
+const contact = resume.contactInfo;
+const education = resume.education;
+const employment = resume.employment;
+const skills = resume.skills;
+
+const commaString = (lst) => {
+    return lst.join(', ');
+}
+
 class Resume extends React.Component {
+
     render() {
         return (
             <Container>
                 <Row>
-                    <Col sm="12" md={{ size: 10, offset: 1 }}>
+                    <Col>
                         <Heading text={"My Resume"}/>
                         <p>If you'd like to take a look at my formal qualifications, I'll throw you a link right <a href="../static/resume.pdf">here.</a></p>
                         <p>I'll also throw in a text version of it below if you're not one for formatting.</p>
-                        <p>TODO: RESUME</p>
+                        <div className="resume-group">
+                            <h3 className="bolded">Contact Info</h3>
+                            <p>{contact.name}</p>
+                            <p>{contact.phone}</p>
+                            <p><a href={'http://www.' + contact.github}>{contact.github}</a></p>
+                            <p><a href={'http://www.' + contact.github}>{contact.linkedin}</a></p>
+                        </div>
+                        <div className="resume-group">
+                            <h3 className="bolded">Education</h3>
+                            <p>{education.school} - {education.location}</p>
+                            <p>{education.degree} expected {education.graduationDate}</p>
+                            <p className="bolded">GPA: {education.gpa}</p>
+                            <p><span className="bolded">Honors: </span>{commaString(education.honors)}</p>
+                            <p><span className="bolded">Activities: </span>{commaString(education.activities)}</p>
+                        </div>
                     </Col>
                 </Row>
             </Container>
