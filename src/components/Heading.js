f044b0e786a4edf4fda52f3acb14d4fb3ecd3c38
mfreema1 <mfreema1@stevens.edu>
2019-01-02 18:13:25 -0500

    New font sizing

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -4,7 +4,7 @@ class Heading extends React.Component {
     render() {
         return (
             <div>
-                <h1 className="big-text special-font pushed-down">{this.props.text}</h1>
+                <h1 className="huge-text special-font pushed-down">{this.props.text}</h1>
                 <hr/>
             </div>
         )
