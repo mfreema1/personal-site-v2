e93ddb6aaa98c39805a43fbcf940783cb6cea467
mfreema1 <mfreema1@stevens.edu>
2019-01-23 16:23:39 -0500

    Change captions

--- src/components/CaptionedImage.js
+++ src/components/CaptionedImage.js
@@ -7,7 +7,10 @@ class CaptionedImage extends React.Component {
                     <img class="small-bottom-space" src={this.props.src}/>
                 </div>
                 <div class="row justify-content-center">
-                    <p class="small-text body-font italics">{this.props.caption}</p>
+                    <p class="small-text body-font italics no-margin-bottom faded">{this.props.caption}</p>
+                </div>
+                <div class="row justify-content-center">
+                    <small class="body-font italics faded small-bottom-space">Photo by <a href={this.props.artistUrl}>{this.props.artistName}</a> on Unsplash</small>
                 </div>
             </div>
         )
