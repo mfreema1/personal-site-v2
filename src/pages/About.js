565e65801fbd558c1500ac8561417887ee3ff06e
Mark <mfreema1@stevens.edu>
2019-01-23 17:09:09 -0500

    Credit photographers

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,12 +14,12 @@ class About extends React.Component {
                             
                             <h4 class="bolded body-font">Humble Beginnings</h4>
                             <p>I grew up on a farm in New Tripoli, Pennsylvania.  In rural PA, there's not too much to do unless you're interested in the usual stuff -- tractors, farming, hunting, etc.  I got into woodworking and machinery early on, but not much of the other stuff.</p>
-                            <CaptionedImage src="/static/field.jpg" caption="Not the exact place, but it's pretty close to the real deal" />
+                            <CaptionedImage src="/static/field.jpg" caption="Not the exact place, but it's pretty close to the real deal" artistUrl="https://unsplash.com/@petritzdesigns" artistName="Markus Petritz"/>
                             <p>I later moved to Allentown and eventually graduated high school there.  I had picked up an appreciation for heavy machinery and physics from my time on the farm, so I decided to attend Stevens Institute of Technology majoring in Mechanical Engineering.</p>
                             
                             <h4 class="bolded body-font">Out of the Frying Pan</h4>
                             <p>Through some pretty incredible professors, I got to see my first line of code, and eventually switched majors to Software Engineering.  I immersed myself as much as possible off the bat -- doing hackathons, joining a startup, and taking online courseware to sharpen my skills.</p>
-                            <CaptionedImage src="/static/computer.jpg" caption="There isn't much that can beat a quiet morning of tea and code" artistUrl="$artistUrl" artistName="$authorName"/>
+                            <CaptionedImage src="/static/computer.jpg" caption="There isn't much that can beat a quiet morning of tea and code" artistUrl="https://unsplash.com/@clemhlrdt" artistName="Clément H"/>
                             <p>This taught me quite a bit about programming, but most importantly, I learned how to teach myself.  Nowadays, I spend my time looking at DevOps, cloud computing, and Agile methodologies.  I also write programs in my free time, and try to pick up new tools whenever possible.</p>
                             
                             <h4 class="bolded body-font">Looking Forward</h4>
@@ -29,7 +29,7 @@ class About extends React.Component {
                                 <li><a href="github.com/mfreema1/food-crawler">MyCookbook</a> - a utility to help people make their own cookbooks from their favorite recipes</li>
                                 <li><a href="github.com/mfreema1/pi-research">Raspberry Flow</a> - a research project I'm working on at my university to help see why people go where they do</li>
                             </ul>
-                            <CaptionedImage src="/static/raspberry.jpg" caption="Now if only we could make Raspberry Flow taste like an actual raspberry flow" />
+                            <CaptionedImage src="/static/raspberry.jpg" caption="Now if only we could make Raspberry Flow taste like an actual raspberry flow" artistUrl="https://unsplash.com/@natalie_rhea" artistName="Natalie Rhea Riggs"/>
                             <p>If you want to check out some of my stuff over at my <a href="github.com/mfreema1">GitHub</a> profile I'd be flattered.  I really enjoy what I do, and I'm always happy to chat about it :)</p>
                         </div>
                     </Col>
