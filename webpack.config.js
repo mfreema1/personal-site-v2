ea6a6704aab803993b19b061090c94681d1b0931
Mark <mfreema1@stevens.edu>
2019-01-23 18:57:20 -0500

    Swap Travis env from production to allow install of dev dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
-    mode: process.env.NODE_ENV == 'production' ? 'production' : 'development',
+    mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
     entry: {
         app: './src/index.js'
     },
