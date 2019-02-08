fd11574a1ebe0fd8718401a5940a718f53a353fa
mfreema1 <mfreema1@stevens.edu>
2019-02-08 13:57:34 -0500

    Fix links on about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -25,9 +25,9 @@ class About extends React.Component {
                             <h4 class="bolded body-font">Looking Forward</h4>
                             <p>There's still a ton left to learn -- in the meantime, I'm just trying to make things that help people.  I tend to switch projects fairly frequently, but the ones I'm working on right now are:</p>
                             <ul class="custom-list bottom-space">
-                                <li><a href="github.com/mfreema1/anonymouse">Anonymouse</a> - an anonymous question service for classrooms</li>
-                                <li><a href="github.com/mfreema1/food-crawler">MyCookbook</a> - a utility to help people make their own cookbooks from their favorite recipes</li>
-                                <li><a href="github.com/mfreema1/pi-research">Raspberry Flow</a> - a research project I'm working on at my university to help see why people go where they do</li>
+                                <li><a href="https://github.com/mfreema1/anonymouse">Anonymouse</a> - an anonymous question service for classrooms</li>
+                                <li><a href="https://github.com/mfreema1/food-crawler">MyCookbook</a> - a utility to help people make their own cookbooks from their favorite recipes</li>
+                                <li><a href="https://github.com/mfreema1/pi-research">Raspberry Flow</a> - a research project I'm working on at my university to help see why people go where they do</li>
                             </ul>
                             <CaptionedImage src="/static/raspberry.jpg" caption="Now if only we could make Raspberry Flow taste like an actual raspberry flow" artistUrl="https://unsplash.com/@natalie_rhea" artistName="Natalie Rhea Riggs"/>
                             <p>If you want to check out some of my stuff over at my <a href="github.com/mfreema1">GitHub</a> profile I'd be flattered.  I really enjoy what I do, and I'm always happy to chat about it :)</p>
