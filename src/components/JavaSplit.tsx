703849779e449cbe0b26024c259ef445a48eb51a
mfreema1 <me@mrkfrmn.com>
2023-12-04 22:46:43 -0500

    Push Java examples into 2 rows on mobile.

--- src/components/JavaSplit.tsx
+++ src/components/JavaSplit.tsx
@@ -6,12 +6,17 @@ export interface JavaSplitProps {
   code: string;
 }
 
+/**
+ * We keep the gutter small here so that when we render on mobile, the gap
+ * between the two rows is smaller than the gap between other elements in the
+ * page stack. This makes it clearer what we are referring to with {@link left}.
+ */
 export function JavaSplit({ left, code }: JavaSplitProps) {
   return (
     <>
-      <Grid w={"100%"}>
-        <Grid.Col span={4}>{left}</Grid.Col>
-        <Grid.Col span={8}>
+      <Grid w={"100%"} gutter="sm">
+        <Grid.Col span={{ base: 12, md: 4 }}>{left}</Grid.Col>
+        <Grid.Col span={{ base: 12, md: 8 }}>
           <JavaSingle code={code} />
         </Grid.Col>
       </Grid>
