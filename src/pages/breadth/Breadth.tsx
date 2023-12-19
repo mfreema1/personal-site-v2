58a81ca7b40185fd1a82e696f5b636a61727a8bf
mfreema1 <me@mrkfrmn.com>
2023-12-18 22:14:27 -0500

    Reformat breadth page.

--- src/pages/breadth/Breadth.tsx
+++ src/pages/breadth/Breadth.tsx
@@ -1,40 +1,36 @@
-import { Text, Title } from "@mantine/core";
 import { Page } from "../../components/Page";
 import { PageTable } from "../../components/PageTable";
-import { Quote } from "../../components/Quote";
+import { TitleSection } from "../../components/TitleSection";
 import { TopPage } from "../../components/pages/TopPage";
+import { BodyText } from "../../components/text/BodyText";
 
 export function Breadth() {
   return (
     <>
       <TopPage>
         <Page path={"/breadth"}>
-          <Title order={1}>
-            <Text span inherit c="blue">
-              Breadth
-            </Text>
-          </Title>
-          <Quote
-            text="The height of the pinnacle is determined by the breadth of the
+          <TitleSection
+            tText="Breadth"
+            qtText="The height of the pinnacle is determined by the breadth of the
               base."
-            author="Ralph Waldo Emerson"
+            qtAuthor="Ralph Waldo Emerson"
           />
 
-          <Text>
+          <BodyText>
             In my experience, problems in Software Engineering tend to have a
             fairly simple solution, but what is simplest may not be what is most
             familiar to us. Unless we intend to treat every problem over our
             career as a nail, we need to get accustomed to tools other than a
             hammer.
-          </Text>
+          </BodyText>
 
-          <Text>
+          <BodyText>
             These pages will cover a broad variety of Software Engineering
             topics. The aim is to provide a birds-eye view of some tools and
             techniques that I have found useful. We will not dive terribly deep
             into each subject, but where possible, I will provide pointers or
             questions for further investigation on your own.
-          </Text>
+          </BodyText>
 
           <PageTable
             rows={[
